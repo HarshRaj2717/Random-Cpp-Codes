ce2f739e23115768c2bf72db1e543d1d232e604b
harshraj2717 <harshraj2717@gmail.com>
2022-08-25 19:42:24 +0530

    created rust borrow checker emulation

---
+++ Emulation of rust borrow checker/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+
+template<typename T>
+auto Move(T var){
+    T moved = std::move(var);
+    delete var;
+    return moved;
+}
+
+
+template<typename T>
+auto borrow(T addr){
+    auto copied = std::move(*addr);
+    return *copied;
+}
+
+
+template<typename T>
+auto mut_borrow(T addr){
+    T taken = addr;
+    return taken;
+}
+
+
+int main(){
+    int* x = new int(33);
+    auto y = borrow(&x);
+    auto z = mut_borrow(&x);
+    *z=123;
+    std::cout << *x << y << **z;
+    auto xx = Move(x);
+
+}
