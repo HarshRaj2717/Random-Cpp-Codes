f7ba83f433579030f6f2e82e09d11d825f4fd530
harshraj2717 <harshraj2717@gmail.com>
2023-03-03 23:42:42 +0530

    made minor changes only for code readability

--- Print 0 to n-1 in a single statement/main.cpp
+++ Print 0 to n-1 in a single statement/main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-    for (int j = 0, i = INT_MAX; j < i; (i == INT_MAX ? (cin >> i) && 1 : j++)) i == INT_MAX ? i = INT_MAX : (cout << j << ' ' && 1);
+    for (int j = 0, i = INT_MAX; j < i; (i == INT_MAX ? ((cin >> i) && 1) : j++)) i == INT_MAX ? i = INT_MAX : ((cout << j << ' ') && 1);
     return 0;
 }
